d730075771fc44af1c10af324827137da188eba0
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2019-07-24 20:44:15 +0200

    created separate file for colors; small fixes

--- clog.go
+++ clog.go
@@ -41,30 +41,6 @@ const escape = "\x1b"
 const textBase = 30
 const backgroundBase = 40
 
-// Base colors
-const (
-	Black int = iota
-	Red
-	Green
-	Yellow
-	Blue
-	Magenta
-	Cyan
-	White
-)
-
-// High intensity colors
-const (
-	HiBlack int = iota + 60
-	HiRed
-	HiGreen
-	HiYellow
-	HiBlue
-	HiMagenta
-	HiCyan
-	HiWhite
-)
-
 // Base attributes
 const (
 	Reset int = iota
@@ -104,7 +80,7 @@ func (w *Writer) SetOutput(writer io.Writer) {
 }
 
 // Write io.Writer implementation.
-func (w *Writer) Write(b []byte) (n int, err error) {
+func (w *Writer) Write(b []byte) (int, error) {
 	c := make([]byte, len(b))
 	copy(c, b)
 	var color string
@@ -126,15 +102,15 @@ func (w *Writer) Write(b []byte) (n int, err error) {
 	}
 
 	if color == "" {
-		w.out.Write(c)
-		return
+		n, err := w.out.Write(c)
+		return n, err
 	}
 
 	w.set(color)
-	w.out.Write(c)
+	n, err := w.out.Write(c)
 	w.unset()
 
-	return len(c), nil
+	return n, err
 }
 
 // Prefix allow to set specific colors which will be set to if prefix will be find in logging text.
@@ -170,11 +146,11 @@ func (w *Writer) Uninstall() {
 
 func (w *Writer) set(c string) {
 	str := fmt.Sprintf("%s[%sm", escape, c)
-	fmt.Fprintf(w.out, str)
+	_, _ = fmt.Fprintf(w.out, str)
 }
 
 func (w *Writer) unset() {
-	fmt.Fprintf(w.out, "%s[%dm", escape, Reset)
+	_, _ = fmt.Fprintf(w.out, "%s[%dm", escape, Reset)
 }
 
 func generate(v ...interface{}) string {
@@ -197,74 +173,3 @@ func generate(v ...interface{}) string {
 
 	return color
 }
-
-// Base colors
-type Colors interface {
-	Black() string
-	Red() string
-	Green() string
-	Yellow() string
-	Blue() string
-	Magenta() string
-	Cyan() string
-	White() string
-
-	HiBlack() string
-	HiRed() string
-	HiGreen() string
-	HiYellow() string
-	HiBlue() string
-	HiMagenta() string
-	HiCyan() string
-	HiWhite() string
-}
-
-type colors struct{}
-
-// Black text color.
-func (c *colors) Black() string { return generate(Black) }
-
-// Red text color.
-func (c *colors) Red() string { return generate(Red) }
-
-// Green text color.
-func (c *colors) Green() string { return generate(Green) }
-
-// Yellow text color.
-func (c *colors) Yellow() string { return generate(Yellow) }
-
-// Blue text color.
-func (c *colors) Blue() string { return generate(Blue) }
-
-// Magenta text color.
-func (c *colors) Magenta() string { return generate(Magenta) }
-
-// Cyan text color.
-func (c *colors) Cyan() string { return generate(Cyan) }
-
-// White text color.
-func (c *colors) White() string { return generate(White) }
-
-// Black high intense color.
-func (c *colors) HiBlack() string { return generate(HiBlack) }
-
-// Red high intense color.
-func (c *colors) HiRed() string { return generate(HiRed) }
-
-// Green high intense color.
-func (c *colors) HiGreen() string { return generate(HiGreen) }
-
-// Yellow high intense color.
-func (c *colors) HiYellow() string { return generate(HiYellow) }
-
-// Blue high intense color.
-func (c *colors) HiBlue() string { return generate(HiBlue) }
-
-// Magenta high intense color.
-func (c *colors) HiMagenta() string { return generate(HiMagenta) }
-
-// Cyan high intense color.
-func (c *colors) HiCyan() string { return generate(HiCyan) }
-
-// White high intense color.
-func (c *colors) HiWhite() string { return generate(HiWhite) }
